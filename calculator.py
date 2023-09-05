892f6a82f44caef2523959f7605192b8a6ee0875
neveremore <117918273+neverwasbored@users.noreply.github.com>
2023-09-05 22:16:19 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Сколько денег вы получаете на данный момент в Suifrens Bullshark Quest 2
-# How much money you will get on this moment at Suifrens Bullshark Quest 2
+# How much money you will get at this moment in Suifrens Bullshark Quest 2
 # Sharks cost 8 because - https://suifrens.com/mint
 
 
